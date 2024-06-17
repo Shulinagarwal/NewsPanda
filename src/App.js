71be909493186b07f3309fe7250246f270a02c52
Shulinagarwal <shulinagarwal2006@gmail.com>
2024-06-17 23:56:49 +0530

    made a new project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <Navbar/>
+      <News/>
+     </div>
+    
+    )
+  }
+}
+
